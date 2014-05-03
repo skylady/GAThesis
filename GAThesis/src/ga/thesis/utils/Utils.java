5ca2cdd2cf07dc83cf08ef85d0bdb3081c82a658
skylady <skylady.mary@gmail.com>
2014-05-03 18:52:43 +0300

    added utils

---
+++ GAThesis/src/ga/thesis/utils/Utils.java
@@ -0,0 +1,16 @@
+package ga.thesis.utils;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Utils {
+	public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
+		for (Entry<T, E> entry : map.entrySet()) {
+			if (value.equals(entry.getValue())) {
+				return entry.getKey();
+			}
+		}
+		return null;
+	}
+}
+
