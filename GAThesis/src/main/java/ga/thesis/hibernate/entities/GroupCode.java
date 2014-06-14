fcbc7dadc211d2a6f03a954903bf996be86069a7
skylady.mary <skylady.mary@gmail.com>
2014-06-14 21:24:15 +0300

    db mapping in progress

--- GAThesis/src/main/java/ga/thesis/hibernate/entities/GroupCode.java
+++ GAThesis/src/main/java/ga/thesis/hibernate/entities/GroupCode.java
@@ -1,7 +1,13 @@
 package ga.thesis.hibernate.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
 /**
  * Created by sky_lady on 6/14/14.
  */
-public class GroupCode {
+@Entity
+@Table(name = "GroupCode")
+public class GroupCode implements Serializable {
 }
--- GAThesis/src/main/java/ga/thesis/hibernate/entities/Teacher.java
+++ GAThesis/src/main/java/ga/thesis/hibernate/entities/Teacher.java
@@ -1,7 +1,37 @@
 package ga.thesis.hibernate.entities;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 /**
  * Created by sky_lady on 6/14/14.
  */
-public class Teacher {
+@Entity
+@Table(name = "Teacher")
+public class Teacher implements Serializable {
+    private static final long serialVersionUID = -5170875020617735673L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "AbsenceMatrix")
+    private AbsenceMatrix idAbsenceMatrix;
+
+    public AbsenceMatrix getIdAbsenceMatrix() {
+        return idAbsenceMatrix;
+    }
+
+    public void setIdAbsenceMatrix(AbsenceMatrix idAbsenceMatrix) {
+        this.idAbsenceMatrix = idAbsenceMatrix;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
